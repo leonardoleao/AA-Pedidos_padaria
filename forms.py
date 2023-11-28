e6b74fc0cd9b8955fd687ef4104a8658b1bb4da2
leonardoleao <43736170+leonardoleao@users.noreply.github.com>
2023-11-27 22:22:29 -0300

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Pedido
-
-class PedidoForm(forms.ModelForm):
-    class Meta:
-        model = Pedido
-        fields = ['produtos', 'quantidade', 'nome_cliente', 'endereco_entrega']
